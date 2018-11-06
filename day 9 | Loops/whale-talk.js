3a0b7f18531711360d6dfd9a52b7b023f6b53c2d
luko <luko.web@gmail.com>
2018-11-06 20:27:05 -0300

    feat: Add 'whale talk' file

---
+++ day 9 | Loops/whale-talk.js
@@ -0,0 +1,36 @@
+
+let input = 'EAGLES is nice and I like it sometimes.';
+input = input.toLowerCase();
+
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+let resultArray = [];
+
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+
+	//console.log(`inputIndex is ${inputIndex}`);
+  for (let vowelsIndex = 0; vowelsIndex < vowels.length; vowelsIndex++ ) {
+
+    //console.log(`vowelsIndex is ${vowelsIndex}`);
+
+    // 
+    if (input[inputIndex] === vowels[vowelsIndex]) {
+      resultArray.push(input[inputIndex])
+    };
+    
+  };
+  
+    if (input[inputIndex] === 'e') {
+			resultArray.push(input[inputIndex]);
+    };
+  
+  	if (input[inputIndex] === 'u') {
+			resultArray.push(input[inputIndex])
+    };
+  
+};
+
+console.log(resultArray.join('').toUpperCase());
+
+
