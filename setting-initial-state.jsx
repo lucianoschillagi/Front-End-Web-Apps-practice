88c9e4a093bc88b97709da919fb9f246a2936c10
luko <luko.web@gmail.com>
2018-12-11 16:29:10 -0300

    Add POST request example (AJAX)

---
+++ setting-initial-state.jsx
@@ -0,0 +1,17 @@
+// React
+// Setting Initial State
+
+// this.state
+class Example extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { mood: 'decent' };
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+<Example />
+
